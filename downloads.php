049c30d9cc03fbae9a1554f5215ebc68eccdfec5
James Kontargyris <james@jameskontargyris.co.uk>
2017-01-26 18:43:51 +0000

    Added event report PDF download

--- downloads.php
+++ downloads.php
@@ -10,6 +10,8 @@
         <li style="padding-top:20px; font-size:17px; line-height:24px;"><strong style="font-size:21px; line-height:24px;">Call to Action: Improving Outcomes for People Living with HIV</strong><br>7 December 2016<br><a href="pdf/hivoutcomes_calltoaction.pdf" class="button--primary" style="display:inline-block; margin-top:10px;">Download as PDF</a></li>
 
         <li style="padding-top:20px; font-size:17px; line-height:24px;"><strong style="font-size:21px; line-height:24px;">Press Release: HIV Outcomes – Beyond Viral Suppression inaugural meeting</strong><br>7 December 2016<br><a href="pdf/hivoutcomes_pressrelease.pdf" class="button--primary" style="display:inline-block; margin-top:10px;">Download as PDF</a></li>
+        
+        <li style="padding-top:20px; font-size:17px; line-height:24px;"><strong style="font-size:21px; line-height:24px;">Event Report: HIV Outcomes – Beyond Viral Suppression inaugural meeting</strong><br>27 January 2017<br><a href="pdf/hivoutcomes_eventreport.pdf" class="button--primary" style="display:inline-block; margin-top:10px;">Download as PDF</a></li>
 
 	</ul>
 </p>
