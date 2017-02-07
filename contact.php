8899f8ec5beb612fab29f8991a97baddf5bb548f
James Kontargyris <james@jameskontargyris.co.uk>
2017-02-07 16:14:20 +0000

    Minor text change

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
     <ul>
         <li><strong>Nikos Dedes</strong>, Chair of HIV Outcomes Advocacy Group - Chair of Positive Voice (the Greek association for PLHIV) and a Board member of the European AIDS Treatment Group (EATG)<br><a href="mailto:nikos.dedes@eatg.org">nikos.dedes@eatg.org</a></li>
         <li><strong>Luis Mendao</strong>, Co-Chair of HIV/AIDS Civil Society Forum (CSF) and Board member of the European AIDS Treatment Group (EATG) <br><a href="mailto:luis.mendao@eatg.org">luis.mendao@eatg.org</a></li>
-        <li><strong>HIV Outcomes Secretariat<br>Rachel Finnegan</strong><br><a href="mailto: rachel.finnegan@fipra.com">rachel.finnegan@fipra.com</a><br>+32 2 613 2828</li>
+        <li><strong>HIV Outcomes Secretariat<br>Rachel Finnegan</strong><br><a href="mailto: hivoutcomes.secretariat@gmail.com">hivoutcomes.secretariat@gmail.com</a><br>+32 2 613 2828</li>
     </ul>
 </p>
 
